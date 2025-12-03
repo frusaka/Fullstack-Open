20135ab5ffa3d160e7bbe64169fb7450eef5ce97
frusaka <fredrick.rusaka@gmail.com>
2025-12-02 18:52:55 -0600

    Complete 2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+export default function App() {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
@@ -47,6 +58,4 @@ const App = (props) => {
       </form>
     </div>
   );
-};
-
-export default App;
+}
